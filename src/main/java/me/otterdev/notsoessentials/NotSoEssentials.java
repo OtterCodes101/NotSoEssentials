b6555ad37d312c7a06814fe47b52ba48ee6f25d5
OtterCodes101 <134523836+OtterCodes101@users.noreply.github.com>
2023-05-29 20:04:57 +0000

    made it actually work as a plugin

---
+++ src/main/java/me/otterdev/notsoessentials/NotSoEssentials.java
@@ -0,0 +1,15 @@
+package me.otterdev.notsoessentials;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NotSoEssentials extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
