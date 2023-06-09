f0f6d9f14a94fad19a66641a6af15706c2809b00
OtterDev <134523836+OtterCodes101@users.noreply.github.com>
2023-06-09 15:09:59 -0600

    Update NotSoEssentials.java

--- src/main/java/me/otterdev/notsoessentials/NotSoEssentials.java
+++ src/main/java/me/otterdev/notsoessentials/NotSoEssentials.java
@@ -1,6 +1,6 @@
 package me.otterdev.notsoessentials;
 
-import org.bukkit.Bukkit;
+import org.bukkit.Bukkit.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NotSoEssentials extends JavaPlugin {
@@ -11,5 +11,7 @@ public class NotSoEssentials extends JavaPlugin {
 
     @Override
     public void onDisable() {
+    // save the server before it stops
+    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "save-all");
     }
 }
